3bfafc456a793e7f0a12b53c8c086f130e48634d
smartthug <rahuldeva5555@gmail.com>
2026-03-17 15:06:56 +0530

    Build error

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,6 @@ const config: CapacitorConfig = {
   appId: "com.habitcracker.app",
   appName: "Habit Cracker",
   webDir: ".next",
-  bundledWebRuntime: false,
   server: {
     // TODO: replace with your actual deployed HTTPS URL on Vercel.
     url: "https://www.indydaze.com",
